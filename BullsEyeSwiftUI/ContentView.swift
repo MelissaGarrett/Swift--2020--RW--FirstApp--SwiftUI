248941e374ef749f55a6bd41af374fe620ba5175
MelissaGarrett <melissa_garrett@hotmail.com>
2020-05-07 19:14:35 -0500

    Added styling to the buttons and slider.

--- BullsEyeSwiftUI/ContentView.swift
+++ BullsEyeSwiftUI/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
     @State var score = 0
     @State var round = 1
     
+    let midnightBlue = Color(red: 0.0/255.0, green: 51.0/255.0, blue: 102.0/255.0)
+    
     struct LabelStyle: ViewModifier {
         func body(content: Content) -> some View {
             return content
@@ -31,6 +33,22 @@ struct ContentView: View {
             .foregroundColor(Color.yellow)
             .modifier(Shadow())
             .font(Font.custom("Arial Rounded MT Bold", size: 24))
+        }
+    }
+    
+    struct ButtonLargeTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 18))
+        }
+    }
+    
+    struct ButtonSmallTextStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 12))
         }
     }
     
@@ -55,7 +73,7 @@ struct ContentView: View {
             // Slider row
             HStack {
                 Text("1").modifier(LabelStyle())
-                Slider(value: $sliderValue, in: 1...100)
+                Slider(value: $sliderValue, in: 1...100).accentColor(Color.green)
                 Text("100").modifier(LabelStyle())
             }
             Spacer()
@@ -65,7 +83,7 @@ struct ContentView: View {
                 print("Button pressed!")
                 self.alertIsVisible = true
             }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/).modifier(ButtonLargeTextStyle())
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
                 return Alert(title: Text(alertTitle()), message: Text("The slider's value is \(sliderValueRounded()).\n" +
@@ -77,6 +95,7 @@ struct ContentView: View {
                     self.round += 1
                     })
             }
+            .background(Image("Button")).modifier(Shadow())
             Spacer()
             
             // Score row
@@ -84,8 +103,13 @@ struct ContentView: View {
                 Button(action: {
                     self.resetGame()
                 }) {
-                    Text("Start over")
+                    HStack {
+                        Image("StartOverIcon")
+                        Text("Start over").modifier(ButtonSmallTextStyle())
+                    }
                 }
+                .background(Image("Button")).modifier(Shadow())
+
                 Spacer()
                 Text("Score:").modifier(LabelStyle())
                 Text("\(score)").modifier(ValueStyle())
@@ -96,12 +120,18 @@ struct ContentView: View {
                 Button(action: {
                     
                 }) {
-                    Text("Info")
+                    HStack {
+                        Image("InfoIcon")
+                        Text("Info").modifier(ButtonSmallTextStyle())
+                    }
                 }
+                .background(Image("Button")).modifier(Shadow())
+
             }
             .padding(.bottom, 20)
         }
         .background(Image("Background"), alignment: .center)
+        .accentColor(midnightBlue)
     }
     
     func sliderValueRounded() -> Int {
